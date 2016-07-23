fbc4a9eb3f1816052a3b0f05476c621c403a70a0
Stig Bertheussen <stigberth@gmail.com>
2016-07-23 08:16:48 +0200

    added reminders to setup.sh to install pip and flake8

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 VUNDLE="https://github.com/VundleVim/Vundle.vim.git"
 VIMDIR=~/.vim
+PIP="https://bootstrap.pypa.io/get-pip.py"
 
 if [ ! -d "$VIMDIR" ]; then
     mkdir -p "$VIMDIR"
@@ -16,3 +17,8 @@ echo "Created symlink: ~/.vimrc -> $(pwd)/vimrc"
 
 read -p "About to install the vim plugins. Vim will complain when it does this because the color scheme isn't installed yet. Just hit enter."
 vim +PluginInstall +qall
+
+echo "###"
+echo "# Don't forget to install pip: curl -O "$PIP" && python get-pip.py"
+echo "# After you have pip, install flake8: pip install flake8"
+echo "###"
