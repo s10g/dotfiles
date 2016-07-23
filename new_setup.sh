dc1a0fc17fc49f330638cacdc61e68e0f864906b
Stig Bertheussen <stigberth@gmail.com>
2016-07-23 09:22:43 +0200

    Made improvements to new_setup.sh

--- new_setup.sh
+++ new_setup.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-#VIMDIR=~/.vim
 PIP="https://bootstrap.pypa.io/get-pip.py"
+VUNDLE=https://github.com/VundleVim/Vundle.vim.git
 
-if [ ! -d "$VIMDIR" ]; then
-    echo ""$VIMDIR" directory not found. Creating directory: "$VIMDIR""
-    mkdir -p "$VIMDIR"
-fi
 
+echo "Cloning Vundle to ~/.vim/bundle/Vundle.vim"
+git clone "$VUNDLE" ~/.vim/bundle/Vundle.vim
 
 echo "Creating symlink: ~/.vimrc -> $(pwd)/vimrc"
 ln -sf $(pwd)/vimrc ~/.vimrc
-
-echo "Creating symlink: ~/.vim -> $(pwd)/vim"
-ln -sf $(pwd)/vim ~/.vim
 
 echo "Creating symlink: ~/.bashrc -> $(pwd)/bashrc"
 ln -sf $(pwd)/bashrc ~/.bashrc
@@ -24,7 +19,12 @@ ln -sf $(pwd)/dir_colors ~/.dir_colors
 echo "Creating symlink: ~/.minttyrc -> $(pwd)/minttyrc"
 ln -sf $(pwd)/minttyrc ~/.minttyrc
 
+echo -e "\e[4mInstalling plugins using Vim (Vundle). Vim will complain about missing color scheme wombat256mod. Ignore and just press Enter.\e[0m"
+read -p "Press Enter."
+vim +PluginInstall
+
 echo ""
 echo "###"
 echo "Don't forget to install pip: (sudo) curl -O "$PIP" && python get-pip.py"
 echo "After you have pip, install flake8: (sudo) pip install flake8"
+
