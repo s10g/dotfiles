d0d894b45a088d9c32561ab33ffd9009ee38bfb5
Stig Bertheussen <stigberth@gmail.com>
2016-07-23 10:19:07 +0200

    removed setup.sh and renamed new_setup.sh as setup.sh

--- setup.sh
+++ setup.sh
@@ -1,40 +1,36 @@
 #!/bin/bash
 
-VUNDLE="https://github.com/VundleVim/Vundle.vim.git"
-VIMDIR=~/.vim
+# Declaring some constants...
 PIP="https://bootstrap.pypa.io/get-pip.py"
-
-if [ ! -d "$VIMDIR" ]; then
-    mkdir -p "$VIMDIR"
-    echo "Created directory: "$VIMDIR""
-fi
+VUNDLE=https://github.com/VundleVim/Vundle.vim.git
 
 
-echo ""
-echo "###"
-echo "# Cloning Vundle into correct location."
-echo "###"
-echo ""
-git clone "$VUNDLE" "$VIMDIR/bundle/Vundle.vim"
+# Making sure we want to run this script...
+echo -e "\e[31mThis script will set up Vim and plugins for you and symlink some .dotfiles (.vimrc, .bashrc, .dir_colors, .minttyrc) in your home. Hit Ctrl-c to abort or Enter to continue...\e[0m"
+read -p ""
 
 
-echo ""
-echo "###"
-echo "# Creating symlink: ~/.vimrc -> $(pwd)/vimrc"
-echo "###"
-echo ""
+# Creating symlinks...
+echo -e "\e[31mCreating symlinks...\e[0m"
+echo "Creating symlink: ~/.vimrc -> $(pwd)/vimrc"
 ln -sf $(pwd)/vimrc ~/.vimrc
+echo "Creating symlink: ~/.bashrc -> $(pwd)/bashrc"
+ln -sf $(pwd)/bashrc ~/.bashrc
+echo "Creating symlink: ~/.dir_colors -> $(pwd)/dir_colors"
+ln -sf $(pwd)/dir_colors ~/.dir_colors
+echo "Creating symlink: ~/.minttyrc -> $(pwd)/minttyrc"
+ln -sf $(pwd)/minttyrc ~/.minttyrc
 
-echo ""
-echo "###"
-read -p "# About to install the vim plugins. Vim will complain when it does this because the color scheme isn't installed yet. Just hit enter."
-echo "###"
-echo ""
-vim +PluginInstall +qall
 
-echo ""
-echo "###"
-echo "# Don't forget to install pip: curl -O "$PIP" && python get-pip.py"
-echo "# After you have pip, install flake8: pip install flake8"
-echo "###"
-echo ""
+# Cloning Vundle...
+echo -e "\e[31mFetching Vundle so we can install Vim plugins...\e[0m"
+git clone "$VUNDLE" ~/.vim/bundle/Vundle.vim
+
+
+# Installing Vim plugins...
+echo -e "\e[31mInstalling plugins using Vim (Vundle). Vim will complain about missing color scheme wombat256mod. Ignore and just press Enter...\e[0m"
+vim +PluginInstall
+
+
+# Finished...
+echo -e "\e[31mFinished...\e[0m"
