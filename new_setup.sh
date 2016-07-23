d18e42d49b97c32b76f1d03f03f020d13b661231
Stig Bertheussen <stigberth@gmail.com>
2016-07-23 09:51:51 +0200

    some improvements to new_setup.sh

--- new_setup.sh
+++ new_setup.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
+# Declaring some constants...
 PIP="https://bootstrap.pypa.io/get-pip.py"
 VUNDLE=https://github.com/VundleVim/Vundle.vim.git
 
 
+# Making sure we want to run this script...
+echo -e "\e[31m--- This script will set up Vim and plugins for you and symlink some .dotfiles in your home. Ctrl-c to abort now if this is not what you want. ---\e[0m"
+read -p ""
 
+
+# Creating symlinks...
 echo -e "\e[31m--- Creating symlinks. ---\e[0m"
 echo "Creating symlink: ~/.vimrc -> $(pwd)/vimrc"
 ln -sf $(pwd)/vimrc ~/.vimrc
-
 echo "Creating symlink: ~/.bashrc -> $(pwd)/bashrc"
 ln -sf $(pwd)/bashrc ~/.bashrc
-
 echo "Creating symlink: ~/.dir_colors -> $(pwd)/dir_colors"
 ln -sf $(pwd)/dir_colors ~/.dir_colors
-
 echo "Creating symlink: ~/.minttyrc -> $(pwd)/minttyrc"
 ln -sf $(pwd)/minttyrc ~/.minttyrc
 
+
+# Cloning Vundle...
 echo -e "\e[31m--- Fetching Vundle so we can install Vim plugins. ---\e[0m"
 git clone "$VUNDLE" ~/.vim/bundle/Vundle.vim
 
+
+# Installing Vim plugins...
 echo -e "\e[31m--- Installing plugins using Vim (Vundle). Vim will complain about missing color scheme wombat256mod. Ignore and just press Enter. ---\e[0m"
 vim +PluginInstall
 
-echo ""
-echo "###"
-echo "Don't forget to install pip: (sudo) curl -O "$PIP" && python get-pip.py"
-echo "After you have pip, install flake8: (sudo) pip install flake8"
 
+# Finished...
+echo -e "\e[31m--- Finished setting up Vim and .dotfiles ---\e[0m"
