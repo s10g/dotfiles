5a50fd3e25fbae8a4dcc6c8740aa60c69cd384a9
Stig Bertheussen <stigberth@gmail.com>
2017-03-14 22:26:19 +0100

    cosmetic changes to setup.sh

--- setup.sh
+++ setup.sh
@@ -32,13 +32,13 @@ then
     git clone "$VUNDLE" "$VUNDLEDIR"
 else
     echo ""$VUNDLEDIR" already exists. Changing directory to "$VUNDLEDIR"..."
-    cd "$VUNDLEDIR"; echo "Now in $(pwd)..."; echo "Pulling..."; git pull; echo "Our work here is done. Going back..."; cd "$OURDIR"; echo "Now in $(pwd)...";
+    cd "$VUNDLEDIR"; echo "Now in $(pwd)..."; echo "Pulling..."; git pull; echo "Our work here is done. Going back..."; cd "$OURDIR"; echo ""; echo "Now in $(pwd)...";
 fi
 
 
 # Installing Vim plugins...
 echo ""
-echo -e "\e[31mInstalling plugins. Vim will complain about missing color scheme wombat256mod. Ignore and just press Enter...\e[0m"
+echo -e "\e[31mInstalling plugins. Vim might complain about missing color scheme. Ignore and just press Enter...\e[0m"
 vim +PluginInstall +qall
 echo "Plugins installed..."
 
