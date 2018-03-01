fc28a7da3cf7b74e746272db7d078e4b9ea658c2
Stig Bertheussen <stigberth@gmail.com>
2018-03-01 23:09:13 +0000

    Change around location of functions for readability.

--- setup.sh
+++ setup.sh
@@ -14,37 +14,6 @@ VIM_BINARY=
 
 
 # FUNCTIONS
-install_vimplug() {
-    # Check for curl version greater than v7.24.0.
-    _curl_version=$(curl --version | head -1 | cut -f 2 -d " ")
-    _curl_main_version=$(echo ${_curl_version} | head -1 | cut -d . -f 1)
-    _curl_release_version=$(echo ${_curl_version} | head -1 | cut -d . -f 2)
-
-    if [ ${_curl_main_version} -ge 7 ] && [ ${_curl_release_version} -ge 24 ]; then
-        :
-    else
-        CURL_OPTIONS=-fLo
-    fi
-
-    # Check for installed *vim type and prioritize Neovim over Vim
-    if [ -e ${LINUX_NVIM_BINARY} ]; then
-        VIM_BINARY=${LINUX_NVIM_BINARY}
-        curl ${CURL_OPTIONS} ${VIMPLUG_NVIM_INSTALL_LOCATION} --create-dirs ${VIMPLUG_URL}
-    elif [ -e ${OPENBSD_NVIM_BINARY} ]; then
-        VIM_BINARY=${OPENBSD_NVIM_BINARY}
-        curl ${CURL_OPTIONS} ${VIMPLUG_NVIM_INSTALL_LOCATION} --create-dirs ${VIMPLUG_URL}
-    elif [ -e ${LINUX_VIM_BINARY} ]; then
-        VIM_BINARY=${LINUX_VIM_BINARY}
-        curl ${CURL_OPTIONS} ${VIMPLUG_VIM_INSTALL_LOCATION} --create-dirs ${VIMPLUG_URL}
-    elif [ -e ${OPENBSD_VIM_BINARY} ]; then
-        VIM_BINARY=${OPENBSD_VIM_BINARY}
-        curl ${CURL_OPTIONS} ${VIMPLUG_VIM_INSTALL_LOCATION} --create-dirs ${VIMPLUG_URL}
-    else
-        echo "Neither Neovim nor Vim found. Ex(c)iting."
-        exit 1
-    fi
-}
-
 prompt() {
 cat <<EOF
 Select which system to set up for
@@ -64,43 +33,6 @@ EOF
         3) setup openbsd;;
         0) exit 0;;
         *) echo ""; echo "Choice not recognized."; echo ""; prompt;;
-    esac
-}
-
-install_symlinks() {
-    case ${1} in
-        cygwin)
-            ln -sf $(pwd)/vimrc ${HOME}/.vimrc
-            ln -sf $(pwd)/minttyrc ${HOME}/.minttyrc
-            ln -sf $(pwd)/bashrc.cygwin ${HOME}/.bashrc
-            ;;
-
-        linux)
-            ln -sf $(pwd)/bashrc.linux ${HOME}/.bashrc
-            if [ -d ${HOME}/.config/nvim ]; then
-                ln -sf $(pwd)/init.vim ${HOME}/.config/nvim/init.vim
-            else
-                mkdir -p ${HOME}/.config/nvim
-                ln -sf $(pwd)/init.vim ${HOME}/.config/nvim/init.vim
-            fi
-            ;;
-
-        openbsd)
-            ln -sf $(pwd)/Xdefaults ${HOME}/.Xdefaults
-            ln -sf $(pwd)/xsession ${HOME}/.xsession
-            ln -sf $(pwd)/profile ${HOME}/.profile
-            if [ -d ${HOME}/.config/nvim ]; then
-                ln -sf $(pwd)/init.vim ${HOME}/.config/nvim/init.vim
-            else
-                mkdir -p ${HOME}/.config/nvim
-                ln -sf $(pwd)/init.vim ${HOME}/.config/nvim/init.vim
-            fi
-            ;;
-
-        *)
-            echo "Unrecognized target to install symlinks for. Exiting."
-            exit 1
-            ;;
     esac
 }
 
@@ -151,6 +83,73 @@ setup() {
     esac
 }
 
+install_symlinks() {
+    case ${1} in
+        cygwin)
+            ln -sf $(pwd)/vimrc ${HOME}/.vimrc
+            ln -sf $(pwd)/minttyrc ${HOME}/.minttyrc
+            ln -sf $(pwd)/bashrc.cygwin ${HOME}/.bashrc
+            ;;
+
+        linux)
+            ln -sf $(pwd)/bashrc.linux ${HOME}/.bashrc
+            if [ -d ${HOME}/.config/nvim ]; then
+                ln -sf $(pwd)/init.vim ${HOME}/.config/nvim/init.vim
+            else
+                mkdir -p ${HOME}/.config/nvim
+                ln -sf $(pwd)/init.vim ${HOME}/.config/nvim/init.vim
+            fi
+            ;;
+
+        openbsd)
+            ln -sf $(pwd)/Xdefaults ${HOME}/.Xdefaults
+            ln -sf $(pwd)/xsession ${HOME}/.xsession
+            ln -sf $(pwd)/profile ${HOME}/.profile
+            if [ -d ${HOME}/.config/nvim ]; then
+                ln -sf $(pwd)/init.vim ${HOME}/.config/nvim/init.vim
+            else
+                mkdir -p ${HOME}/.config/nvim
+                ln -sf $(pwd)/init.vim ${HOME}/.config/nvim/init.vim
+            fi
+            ;;
+
+        *)
+            echo "Unrecognized target to install symlinks for. Exiting."
+            exit 1
+            ;;
+    esac
+}
+
+install_vimplug() {
+    # Check for curl version greater than v7.24.0.
+    _curl_version=$(curl --version | head -1 | cut -f 2 -d " ")
+    _curl_main_version=$(echo ${_curl_version} | head -1 | cut -d . -f 1)
+    _curl_release_version=$(echo ${_curl_version} | head -1 | cut -d . -f 2)
+
+    if [ ${_curl_main_version} -ge 7 ] && [ ${_curl_release_version} -ge 24 ]; then
+        :
+    else
+        CURL_OPTIONS=-fLo
+    fi
+
+    # Check for installed *vim type and prioritize Neovim over Vim
+    if [ -e ${LINUX_NVIM_BINARY} ]; then
+        VIM_BINARY=${LINUX_NVIM_BINARY}
+        curl ${CURL_OPTIONS} ${VIMPLUG_NVIM_INSTALL_LOCATION} --create-dirs ${VIMPLUG_URL}
+    elif [ -e ${OPENBSD_NVIM_BINARY} ]; then
+        VIM_BINARY=${OPENBSD_NVIM_BINARY}
+        curl ${CURL_OPTIONS} ${VIMPLUG_NVIM_INSTALL_LOCATION} --create-dirs ${VIMPLUG_URL}
+    elif [ -e ${LINUX_VIM_BINARY} ]; then
+        VIM_BINARY=${LINUX_VIM_BINARY}
+        curl ${CURL_OPTIONS} ${VIMPLUG_VIM_INSTALL_LOCATION} --create-dirs ${VIMPLUG_URL}
+    elif [ -e ${OPENBSD_VIM_BINARY} ]; then
+        VIM_BINARY=${OPENBSD_VIM_BINARY}
+        curl ${CURL_OPTIONS} ${VIMPLUG_VIM_INSTALL_LOCATION} --create-dirs ${VIMPLUG_URL}
+    else
+        echo "Neither Neovim nor Vim found. Ex(c)iting."
+        exit 1
+    fi
+}
 
 # MAIN
 if [ $# -eq 0 ]; then
